709766aa509d0c68a52ff55083c3325502a6316d
Gachikoi <k1ss.sakura@outlook.com>
2026-02-05 20:01:25 +0800

    chore(web): 测试 service worker 更新是否生效

--- packages/web/src/routes/app/+page.svelte
+++ packages/web/src/routes/app/+page.svelte
@@ -4,7 +4,7 @@
 	import { Menu } from 'lucide-svelte';
 </script>
 
-<div>首页，测试chrome</div>
+<div>s首页，测试手机端</div>
 <Button variant="default">删除评论<Menu /></Button>
 <Button variant="secondary">私信</Button>
 <Button variant="tertiary">删除评论</Button>
