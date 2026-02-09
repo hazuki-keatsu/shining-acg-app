eb3173693687dd58ec56ac96a51285a6c1e37c37
Thomaswadee <919331952@qq.com>
2026-02-09 16:12:44 +0800

    feat: 黑夜模式

--- packages/web/src/routes/app/notification/components/management-nav.svelte
+++ packages/web/src/routes/app/notification/components/management-nav.svelte
@@ -29,7 +29,7 @@
 				'flex h-10 w-24 items-center justify-center rounded-full px-4 py-2 text-base whitespace-nowrap transition-colors',
 				isActive
 					? 'bg-zinc-100 font-semibold text-zinc-900 dark:bg-zinc-800 dark:text-zinc-100'
-					: 'font-normal text-zinc-900 hover:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-800'
+					: 'font-normal text-zinc-900 hover:bg-zinc-50 dark:text-zinc-100 dark:hover:bg-zinc-800'
 			)}
 		>
 			{item.label}
